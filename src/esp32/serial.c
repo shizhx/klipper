73806ea563a61e14991be44e63afd07b8358db30
shizhx <main.shizhi@gmail.com>
2022-06-15 22:14:51 +0800

    esp32: fix compile error

--- src/esp32/serial.c
+++ src/esp32/serial.c
@@ -5,3 +5,9 @@
  *
  * This file may be distributed under the terms of the GNU GPLv3 license.
  */
+#include "board/serial_irq.h"
+
+// callback provided by board specific code
+void serial_enable_tx_irq(void)
+{
+}
--- src/esp32/timer.c
+++ src/esp32/timer.c
@@ -1,19 +1,20 @@
 /**
  * timer support on ESP32 series processors except ESP32
- * 
+ *
  * Copyright (C) 2022 Shizhi Huang <main.shizhi@gmail.com>
- * 
+ *
  * This file may be distributed under the terms of the GNU GPLv3 license.
  */
 
+#include "board/irq.h"
 #include "board/misc.h"
 
 /**
  * @brief return true if time1 is before timer2.
- * 
+ *
  * @param time1
- * @param time2 
- * @return uint8_t 
+ * @param time2
+ * @return uint8_t
  */
 uint8_t timer_is_before(uint32_t time1, uint32_t time2)
 {
@@ -22,5 +23,36 @@ uint8_t timer_is_before(uint32_t time1, uint32_t time2)
 
 void timer_kick(void)
 {
+}
 
+// Disable hardware interrupts
+void irq_disable(void)
+{
+}
+
+// Enable hardware interrupts
+void irq_enable(void)
+{
+}
+
+// Disable hardware interrupts in not already disabled
+irqstatus_t irq_save(void)
+{
+    return 0;
+}
+
+// Restore hardware interrupts to state from flag returned by irq_save()
+void irq_restore(irqstatus_t flag)
+{
+}
+
+// Atomically enable hardware interrupts and sleep processor until next irq
+void irq_wait(void)
+{
+}
+
+// Check if an interrupt is active (used only on architectures that do
+// not have hardware interrupts)
+void irq_poll(void)
+{
 }
